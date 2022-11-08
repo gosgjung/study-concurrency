b228d6d9f1841f3f5105f6e35e3961062d62460e
soon.good <soon.good@outlook.com>
2022-11-08 21:02:03 +0900

    프로젝트 초기세팅

---
+++ WITH-PRODUCT-STOCK-SYSTEM-EXAMPLE/product_stock/src/test/java/io/study/cuncurrency/product_stock/ProductStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.cuncurrency.product_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
