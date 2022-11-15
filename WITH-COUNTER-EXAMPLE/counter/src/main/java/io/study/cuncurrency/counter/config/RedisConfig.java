d856e222d3813d3901c08d5af0e7c88da89705f3
gosgjung <gosgjung@gmail.com>
2022-11-15 15:43:20 +0900

    redis 의존성, 접속주소 추가

---
+++ WITH-COUNTER-EXAMPLE/counter/src/main/java/io/study/cuncurrency/counter/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package io.study.cuncurrency.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class RedisConfig {
+
+//    @Value("${spring.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.redis.port}")
+//    private int port;
+
+
+}
